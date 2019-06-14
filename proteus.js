eea792d1daacece88f11df37d635bfc2aa97ae5b
Amarius1 <amarius2000@gmail.com>
2019-06-14 16:11:27 +0300

    Add files via upload

--- proteus.js
+++ proteus.js
@@ -552,25 +552,22 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-var acc2 = document.querySelectorAll('a[expand]');
+var coll = document.querySelectorAll('[accordion]');
 var i;
 
-for (i = 0; i < acc2.length; i++) {
-  acc2[i].addEventListener("click", function() {
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-      panel.style.opacity = "0";
-      panel.style.visibility = "hidden";
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.style.opacity = "1";
-      panel.style.visibility = "visible";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
 $("a[btn]").addClass("ripple");
+$("a[accordion]").addClass("ripple");
 $(".tabs > span[action] > label").addClass("ripple");
 $(".dropdown > label").addClass("ripple");
 $(".dropdown li").addClass("ripple");
